2c82cc4855df34b88b87bbf34d0df93e7f1a0439
Ivona Mircheva <ivona1mircheva@gmail.com>
2018-06-15 10:44:50 +0300

    Basic steps

---
+++ 1/src/mysql/project/Main.java
@@ -0,0 +1,9 @@
+package mysql.project;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MySQLAccess exampleUse = new MySQLAccess();
+        exampleUse.readDataBase();
+    }
+
+}
